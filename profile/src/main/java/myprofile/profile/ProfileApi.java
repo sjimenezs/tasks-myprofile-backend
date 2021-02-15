f06f887a38e61baa267a371c5830a6b8371e7e0e
Samuel Jimenez <samuel@samueljimenez.co>
2021-02-14 21:53:50 -0500

    cors production

--- profile/src/main/java/myprofile/profile/ProfileApi.java
+++ profile/src/main/java/myprofile/profile/ProfileApi.java
@@ -1,13 +1,12 @@
 package myprofile.profile;
 
-import myprofile.common.context.ExecutionContext;
 import myprofile.common.dto.CheckUserRequest;
 import myprofile.common.dto.CheckUserResponse;
 import myprofile.common.result.Result;
 import myprofile.services.ProfileServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = "https://demo.samueljimenez.co", maxAge = 3600)
 @RestController()
 @RequestMapping("profile")
 public class ProfileApi {
